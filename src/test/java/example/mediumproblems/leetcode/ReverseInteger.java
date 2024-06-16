332e50835923e3b652a29f4b57bae5eb72b335d2
Chandrakant Verma <chandra.verma196@gmail.com>
2024-06-17 03:41:59 +0530

    corrected Reverse Integer

--- src/test/java/example/mediumproblems/leetcode/ReverseInteger.java
+++ src/test/java/example/mediumproblems/leetcode/ReverseInteger.java
@@ -1,14 +1,26 @@
 package example.mediumproblems.leetcode;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ReverseInteger {
+    @Test
+    public void testReverse(){
+        System.out.println(reverse(-2147483648));
+    }
+
     public int reverse(int x) {
-        boolean neg = x<0;
-        if(neg){
-            x = -1*x;
-        }
+        //if (x > Integer.MAX_VALUE || x < Integer.MIN_VALUE) return 0;
+        boolean neg = x < 0;
+
         String s = String.valueOf(x);
         StringBuilder sb = new StringBuilder(s);
         s = sb.reverse().toString();
-        return (neg?-1:1)*Integer.valueOf(s);
+        if(neg)
+            s = s.substring(0,s.length()-1);
+        long value = (neg ? -1 : 1) * Long.valueOf(s);
+        if (value > Integer.MAX_VALUE || value < Integer.MIN_VALUE) return 0;
+
+        return (int) value;
     }
 }
